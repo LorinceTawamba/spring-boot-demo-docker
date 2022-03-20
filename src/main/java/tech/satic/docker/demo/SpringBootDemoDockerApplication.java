fbbd971183579f1cd1e8b3ce5e65d9fd87114af1
LorinceTawamba <lorince.tawamba@gmail.com>
2022-03-20 09:23:42 +0100

    Création du projet

---
+++ src/main/java/tech/satic/docker/demo/SpringBootDemoDockerApplication.java
@@ -0,0 +1,13 @@
+package tech.satic.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoDockerApplication.class, args);
+    }
+
+}
